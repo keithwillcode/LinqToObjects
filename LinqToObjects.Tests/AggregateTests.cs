e4ea05098c777016c72f76e5bc70622cf29f6bb2
Keith Williams <kawillia@vt.edu>
2012-12-18 16:27:46 -0500

    Implemented the Aggregate method.

---
+++ LinqToObjects.Tests/AggregateTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LinqToObjects.Tests
+{
+    [TestFixture]
+    public class AggregateTests
+    {
+        [TestFixture]
+        public class DefaultTests
+        {
+            [Test]
+            public void ArgumentNullExceptionIsThrownWhenSourceIsNull()
+            {
+                Assert.Throws<ArgumentNullException>(() => Enumerable.Aggregate<Int32>(null, (i, j) => i));
+            }
+
+            [Test]
+            public void ArgumentNullExceptionIsThrownWhenFuncIsNull()
+            {
+                Func<Int32, Int32, Int32> func = null;
+                Assert.Throws<ArgumentNullException>(() => Enumerable.Aggregate<Int32>(Enumerable.Empty<Int32>(), func));
+            }
+
+            [Test]
+            public void InvalidOperationExceptionIsThrownWhenSourceContainsNoElements()
+            {
+                Assert.Throws<InvalidOperationException>(() => Enumerable.Aggregate<Int32>(Enumerable.Empty<Int32>(), (i, j) => i));
+            }
+
+            [Test]
+            public void FuncIsAppliedToAllElementsInTheSequence()
+            {
+                var sentence = "the quick brown fox jumps over the lazy dog";
+                var words = sentence.Split(' ');
+                var reversed = words.Aggregate((workingSentence, next) => next + " " + workingSentence);
+
+                Assert.That(reversed, Is.EqualTo("dog lazy the over jumps fox brown quick the "));
+            }
+        }
+
+        [TestFixture]
+        public class SeedTests
+        {
+            [Test]
+            public void ArgumentNullExceptionIsThrownWhenSourceIsNull()
+            {
+                Assert.Throws<ArgumentNullException>(() => Enumerable.Aggregate<Int32, Double>(null, 0.0d, (i, j) => i));
+            }
+
+            [Test]
+            public void ArgumentNullExceptionIsThrownWhenFuncIsNull()
+            {
+                Func<Double, Int32, Double> func = null;
+                Assert.Throws<ArgumentNullException>(() => Enumerable.Aggregate<Int32, Double>(Enumerable.Empty<Int32>(), 0.0d, func));
+            }
+
+            [Test]
+            public void FuncIsAppliedToAllElementsInTheSequence()
+            {
+                var ints = new[] { 4, 8, 8, 3, 9, 0, 7, 8, 2 };
+                var numEven = ints.Aggregate(0, (total, next) => next % 2 == 0 ? total + 1 : total);
+
+                Assert.That(numEven, Is.EqualTo(6));
+            }
+        }
+
+        [TestFixture]
+        public class SelectorTests
+        {
+            [Test]
+            public void ArgumentNullExceptionIsThrownWhenSourceIsNull()
+            {
+                Assert.Throws<ArgumentNullException>(() => Enumerable.Aggregate<Int32, Double, String>(null, 0.0d, (i, j) => i, i => Convert.ToString(i)));
+            }
+
+            [Test]
+            public void ArgumentNullExceptionIsThrownWhenFuncIsNull()
+            {
+                Func<Double, Int32, Double> func = null;
+                Assert.Throws<ArgumentNullException>(() => Enumerable.Aggregate<Int32, Double, String>(Enumerable.Empty<Int32>(), 0.0d, func, i => Convert.ToString(i)));
+            }
+
+            [Test]
+            public void ArgumentNullExceptionIsThrownWhenResultSelectorIsNull()
+            {
+                Func<Double, String> resultSelector = null;
+                Assert.Throws<ArgumentNullException>(() => Enumerable.Aggregate<Int32, Double, String>(Enumerable.Empty<Int32>(), 0.0d, (i, j) => i, resultSelector));
+            }
+
+            [Test]
+            public void FuncIsAppliedToAllElementsInTheSequence()
+            {
+                var fruits = new[] { "apple", "mango", "orange", "passionfruit", "grape" };
+                var longestName = fruits.Aggregate("banana", (longest, next) => next.Length > longest.Length ? next : longest, fruit => fruit.ToUpper());
+
+                Assert.That(longestName, Is.EqualTo("PASSIONFRUIT"));
+            }
+        }
+    }
+}
---
+++ LinqToObjects/Aggregate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        public static TSource Aggregate<TSource>(this IEnumerable<TSource> source, Func<TSource, TSource, TSource> func)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (func == null)
+                throw new ArgumentNullException("func");
+
+            var found = false;
+            var aggregateValue = default(TSource);
+
+            foreach (var element in source)
+            {
+                found = true;
+                aggregateValue = func(aggregateValue, element);
+            }
+
+            if (!found)
+                throw new InvalidOperationException("source contains no elements.");
+
+            return aggregateValue;
+        }
+
+        public static TAccumulate Aggregate<TSource, TAccumulate>(this IEnumerable<TSource> source, TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> func)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (func == null)
+                throw new ArgumentNullException("func");
+
+            var aggregateValue = seed;
+
+            foreach (var element in source)
+                aggregateValue = func(aggregateValue, element);
+
+            return aggregateValue;
+        }
+
+        public static TResult Aggregate<TSource, TAccumulate, TResult>(this IEnumerable<TSource> source, TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> func, Func<TAccumulate, TResult> resultSelector)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (func == null)
+                throw new ArgumentNullException("func");
+
+            if (resultSelector == null)
+                throw new ArgumentNullException("resultSelector");
+
+            var aggregateValue = seed;
+
+            foreach (var element in source)
+                aggregateValue = func(aggregateValue, element);
+
+            return resultSelector(aggregateValue);
+        }
+    }
+}
