daa029bc540482e8d2e1803c0e403646fd8c6f69
Keith Williams <kawillia@vt.edu>
2012-12-17 19:01:15 -0500

    Implemented the Take method.

---
+++ LinqToObjects.Tests/TakeTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinqToObjects.Tests
+{
+    [TestClass]
+    public class TakeTests
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void ArgumentNullExceptionIsThrownWhenSourceIsNull()
+        {
+            Enumerable.Take<Int32>(null, 1);
+        }
+
+        [TestMethod]
+        public void EmptySequenceIsReturnedWhenCountIsLessThanOrEqualToZero()
+        {
+            var source = new[] { 1, 2, 3 };
+            var result = source.Take(0);
+
+            Assert.IsTrue(System.Linq.Enumerable.SequenceEqual(Enumerable.Empty<Int32>(), result));
+        }
+
+        [TestMethod]
+        public void CountElementsAreReturnedWhenCountIsLessThanNumberOfElements()
+        {
+            var source = new[] { 1, 2, 3 };
+            var result = source.Take(2);
+            var expected = new[] { 1, 2 };
+
+            Assert.IsTrue(System.Linq.Enumerable.SequenceEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void AllElementsAreReturnedWhenCountIsGreaterThanNumberOfElements()
+        {
+            var source = new[] { 1, 2, 3 };
+            var result = source.Take(6);
+            var expected = new[] { 1, 2, 3 };
+
+            Assert.IsTrue(System.Linq.Enumerable.SequenceEqual(expected, result));
+        }
+    }
+}
---
+++ LinqToObjects/Take.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Take<TSource>(this IEnumerable<TSource> source, Int32 count)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (count <= 0)
+                return Enumerable.Empty<TSource>();
+
+            return RunTakeAlgorithm(source, count);
+        }
+
+        private static IEnumerable<TSource> RunTakeAlgorithm<TSource>(this IEnumerable<TSource> source, Int32 count)
+        {
+            var numberTaken = 0;
+
+            foreach (var element in source)
+            {
+                yield return element;
+
+                if (++numberTaken == count)
+                    yield break;
+            }
+        }
+    }
+}
