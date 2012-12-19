bde7398e8a6f7411d86832c2680f3816f22a3d6a
Keith Williams <kawillia@vt.edu>
2012-12-19 10:59:54 -0500

    Implemented the Contains method.

---
+++ LinqToObjects.Tests/ContainsTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace LinqToObjects.Tests
+{
+    [TestFixture]
+    public class ContainsTests
+    {
+        [TestFixture]
+        public class DefaultTests
+        {
+            [Test]
+            public void ArgumentNullExceptionIsThrownWhenSourceIsNull()
+            {
+                Assert.Throws<ArgumentNullException>(() => Enumerable.Contains<Int32>(null, 0));
+            }
+
+            [Test]
+            public void ReturnsTrueWhenElementIsInSource()
+            {
+                var source = new[] { 1, 2, 3 };
+                Assert.That(source.Contains(2), Is.True);
+            }
+
+            [Test]
+            public void ReturnsFalseWhenElementIsNotInSource()
+            {
+                var source = new[] { 1, 2, 3 };
+                Assert.That(source.Contains(4), Is.False);
+            }
+        }
+
+        [TestFixture]
+        public class EqualityComparerTests
+        {
+            [Test]
+            public void ArgumentNullExceptionIsThrownWhenSourceIsNull()
+            {
+                Assert.Throws<ArgumentNullException>(() => Enumerable.Contains<Int32>(null, 0, EqualityComparer<Int32>.Default));
+            }
+
+            [Test]
+            public void ReturnsTrueWhenElementIsInSource()
+            {
+                var source = new[] { 1, 2, 3 };
+                Assert.That(source.Contains(2, EqualityComparer<Int32>.Default), Is.True);
+            }
+
+            [Test]
+            public void ReturnsFalseWhenElementIsNotInSource()
+            {
+                var source = new[] { 1, 2, 3 };
+                Assert.That(source.Contains(4, EqualityComparer<Int32>.Default), Is.False);
+            }
+        }
+    }
+}
---
+++ LinqToObjects/Contains.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqToObjects
+{
+    public static partial class Enumerable
+    {
+        public static Boolean Contains<TSource>(this IEnumerable<TSource> source, TSource value)
+        {
+            return Contains(source, value, EqualityComparer<TSource>.Default);
+        }
+
+        public static Boolean Contains<TSource>(this IEnumerable<TSource> source, TSource value, IEqualityComparer<TSource> comparer)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            if (comparer == null)
+                comparer = EqualityComparer<TSource>.Default;
+
+            return RunContainsAlgorithm(source, value, comparer);
+        }
+
+        private static Boolean RunContainsAlgorithm<TSource>(IEnumerable<TSource> source, TSource value, IEqualityComparer<TSource> comparer)
+        {
+            foreach (var element in source)
+                if (comparer.Equals(element, value))
+                    return true;
+
+            return false;
+        }
+    }
+}
